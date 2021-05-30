d2166d18f689c2b3bd9d0d85ce097b7e3bc84140
S0und16 <nawillsher@gmail.com>
2021-05-30 19:50:26 -0400

    Added proper initialization, texture loading and texture display via renderer

--- WLUWGameEngine/main.cpp
+++ WLUWGameEngine/main.cpp
@@ -3,18 +3,76 @@
 constexpr int SCREEN_WIDTH = 640;
 constexpr int SCREEN_HEIGHT = 480;
 
+// Initialize SDL
+bool init()
+{
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	// Initialize SDL_image
+	int imageFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imageFlags) & imageFlags))
+	{
+		printf("SDL_image could not initialize! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+/* Loads texture at the given path
+* path: the image location relative to the solution
+* renderer: the renderer which will be rendering the texture
+*/
+SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer)
+{
+	// Final texture
+	SDL_Texture* newText = NULL;
+
+	// Load image to surface
+	SDL_Surface* loadedImg = IMG_Load(path.c_str());
+	// Convert surface to texture
+	newText = SDL_CreateTextureFromSurface(renderer, loadedImg);
+
+	// TODO: Idiotproof
+
+	SDL_FreeSurface(loadedImg);
+	return newText;
+}
+
 int main(int argc, char* argv[])
 {
-	SDL_Window* window = SDL_CreateWindow("WLUW Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN & SDL_WINDOW_MOUSE_FOCUS);
+	if (!init())
+	{
+		return 1;
+	}
 
+	SDL_Window* window = SDL_CreateWindow("WLUW Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN & SDL_WINDOW_MOUSE_FOCUS);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_Texture* texture = loadTexture("assets/loaded.png", renderer);
+
+	// Allows the program to exit
 	bool quit = false;
 
+	// For event handling
 	SDL_Event e;
 
+	// Main game loop
 	while (!quit)
 	{
-		while (SDL_PollEvent(&e))
+		// Fill the screen with a solid colour (white)
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+
+		// Poll any user events
+		while (SDL_PollEvent(&e) != 0)
 		{
+			// User clicks 'X' button
 			if (e.type == SDL_QUIT)
 			{
 				quit = true;
@@ -29,9 +87,18 @@ int main(int argc, char* argv[])
 				}
 			}
 		}
+
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+		// Update the screen
+		SDL_RenderPresent(renderer);
 	}
 
+	// Cleanup
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	IMG_Quit();
+	SDL_Quit();
 
 	return 0;
 }
