2cad6a7567ad4813b5b2180797d88db7bd941423
S0und16 <nawillsher@gmail.com>
2021-05-30 00:22:27 -0400

    Linked SDL2 and Created Basic Window
    Self Explanatory

---
+++ WLUWGameEngine/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "SDL.h"
+
+constexpr int SCREEN_WIDTH = 640;
+constexpr int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* argv[])
+{
+	SDL_Window* window = SDL_CreateWindow("WLUW Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN & SDL_WINDOW_MOUSE_FOCUS);
+
+	SDL_Delay(2000);
+
+	return 0;
+}
