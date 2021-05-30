a4288041d880f158e06ff9a58e4bcfd4aed62889
S0und16 <nawillsher@gmail.com>
2021-05-30 13:45:22 -0400

    Added Event Detection for exiting the window
    Added main for loop, event detection, and parameters for exiting program

--- WLUWGameEngine/main.cpp
+++ WLUWGameEngine/main.cpp
@@ -7,7 +7,31 @@ int main(int argc, char* argv[])
 {
 	SDL_Window* window = SDL_CreateWindow("WLUW Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN & SDL_WINDOW_MOUSE_FOCUS);
 
-	SDL_Delay(2000);
+	bool quit = false;
+
+	SDL_Event e;
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+			else if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					quit = true;
+					break;
+				}
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 
 	return 0;
 }
