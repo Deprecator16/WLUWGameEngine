63b4b31cdd08a25b20c823ff7b85020f7a16b963
S0und16 <nawillsher@gmail.com>
2021-05-31 21:23:09 -0400

    Added Vector2 class

---
+++ WLUWGameEngine/Vector2.h
@@ -0,0 +1,77 @@
+#pragma once
+class Vector2
+{
+private:
+	float x;
+	float y;
+
+public:
+	friend Vector2 operator+(const Vector2 vec1, const Vector2 vec2)
+	{
+		Vector2 output;
+		output.x = vec1.x + vec2.x;
+		output.y = vec1.y + vec2.y;
+		return output;
+	}
+
+	friend Vector2 operator-(const Vector2 vec1, const Vector2 vec2)
+	{
+		Vector2 output;
+		output.x = vec1.x - vec2.x;
+		output.y = vec1.y - vec2.y;
+		return output;
+	}
+
+	friend Vector2 operator*(const Vector2 vec1, const Vector2 vec2)
+	{
+		Vector2 output;
+		output.x = vec1.x * vec2.x;
+		output.y = vec1.y * vec2.y;
+		return output;
+	}
+
+	friend Vector2 operator/(const Vector2 vec1, const Vector2 vec2)
+	{
+		Vector2 output;
+		output.x = vec1.x / vec2.x;
+		output.y = vec1.y / vec2.y;
+		return output;
+	}
+
+	float& operator[](int index)
+	{
+		if (index > 1 || index < 0)
+		{
+			throw("out of range");
+			float x;
+			return x;
+		}
+		else if (index == 0)
+		{
+			return x;
+		}
+		else if (index == 1)
+		{
+			return y;
+		}
+	}
+
+	const float& operator[](int index) const
+	{
+		if (index > 1 || index < 0)
+		{
+			throw("out of range");
+			float x;
+			return x;
+		}
+		else if (index == 0)
+		{
+			return x;
+		}
+		else if (index == 1)
+		{
+			return y;
+		}
+	}
+};
+
