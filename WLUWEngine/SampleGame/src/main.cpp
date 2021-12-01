eaebefcc2873dcc78e0172e4d4cee6f090bceb0d
Bassil Virk <bassillooney@outlook.com>
2021-12-01 13:45:29 -0500

    Moved SDL out to solution folder
    SDL was moved out because of library linking issues (Stackoverflow is dumb).

--- WLUWEngine/SampleGame/src/main.cpp
+++ WLUWEngine/SampleGame/src/main.cpp
@@ -1,11 +1,10 @@
-#include "WLUW.h"
 #include "WWindow.h"
 #include <cstdlib>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	WWindow win;
 	system("pause");
 	return 0;
 }
