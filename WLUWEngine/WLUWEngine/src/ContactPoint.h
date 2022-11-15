49555543e863fa2077e2d2299b6f41ac809a9596
ThingBlue <thingbluex@gmail.com>
2022-11-15 00:44:59 -0500

    Initial commit

---
+++ WLUWEngine/WLUWEngine/src/ContactPoint.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Edge.h"
+#include "Hitbox.h"
+#include "Vector2.h"
+
+namespace WLUW
+{
+	struct ContactPoint
+	{
+	public:
+		Vector2 point;
+		Edge edge;
+		Vector2 normal;
+		Vector2 distance;
+		Vector2 relativeVel;
+		Hitbox* box;
+		Hitbox* otherBox;
+
+	};
+}
